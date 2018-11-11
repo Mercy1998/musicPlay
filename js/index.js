6534683eb65c44d2887a27707944757f86517aed
Mercy1998 <1143776778@qq.com>
2018-11-11 14:45:13 +0800

    input[type=range]

--- js/index.js
+++ js/index.js
@@ -14,47 +14,65 @@ var duration;  //总时长
 var current;
 var currentTime;     //$对象
 var fullTime;      //$对象
-var musicList=[
-    {
-        "sing":"太早",
-        "singer":"刘允乐",
-       // "songPic":""  专辑封面
-        "src":"res/audio/testSong.mp3"
-    },{
-        "sing":"我的梦",
-        "singer":"张靓颖",
-        "src":"res/audio/testSong2.mp3"
-    },
-    {
-        "sing":"Mercy",
-        "singer":"未知",
-        "src":"res/audio/Mercy.mp3"
-    }
-];
+var totalBar;
+var progressBar;
+// var musicList=[
+//     {
+//         "sing":"太早",
+//         "singer":"刘允乐",
+//        // "songPic":""  专辑封面
+//         "src":"res/audio/testSong.mp3"
+//     },{
+//         "sing":"我的梦",
+//         "singer":"张靓颖",
+//         "src":"res/audio/testSong2.mp3"
+//     },
+//     {
+//         "sing":"Mercy",
+//         "singer":"未知",
+//         "src":"res/audio/Mercy.mp3"
+//     }
+// ];
 var songName;
 var singerName;
 var songIndex=0;
+var total=null;
+var musicLists=null;
+
+//加载JSON数据
+$.getJSON('musicLists.json',function(data){
+    total=data.data.length;
+    console.log(total);
+    musicLists=data.data;
+    console.log(musicLists[songIndex].sing);
+    $(document).ready();
+})
 
 
 $(document).ready(function(){
 
 /*歌手、歌名*/
+
        songName=$('#sing');
     singerName=$('#singer');
 
     /*歌曲列表*/
     createMusicList();
-    $('#listTitle').text("播放列表 ("+musicList.length+")");
+    $('#listTitle').text("播放列表 ("+total+")");
     /*时间*/
     currentTime=$('#currentTime');
     fullTime=$('#totalTime');
+
+    totalBar=$('#totalBar');
+    progressBar=document.getElementById('totalBar');
+
 
     audio=document.getElementById('music');
     audioJq=$('#music');
     audio.autoplay=true;
 
     /*打开页面播放第一首歌*/
-    loadFirstSong();
+   loadSong();
 
     //音乐控制播放
     $('#controlBtn').click(function(){
@@ -74,37 +92,37 @@ $(document).ready(function(){
 
     //下一首
     $('#nextBtn').click(function(){
-            if(songIndex==musicList.length-1){
-                alert("当前是最后一首歌曲");
+            if(songIndex==total-1){
+               songIndex=0;
 
             }
         else {
                 songIndex++;
+            }
                 console.log(songIndex);
                 songEnded();
-                changeSong();
+                loadSong();
                 audio.load();
                 audio.play();
                 document.getElementById('songPic').style.animationPlayState='running';       //音乐开始
             }
-    }
-    )
+    );
     //上一首
         $('#preBtn').click(function(){
             if(songIndex==0){
-                alert("当前是第一首歌曲");
+               songIndex=total-1;
 
             }
-            else {
+            else{
                 songIndex--;
+            }
                 console.log(songIndex);
                 songEnded();
-                changeSong();
+                loadSong();
                 audio.load();
                 audio.play();
                 document.getElementById('songPic').style.animationPlayState='running';       //音乐开始
-            }
-        })
+        });
 
 //键盘事件
     $(document).keydown(function(e){
@@ -127,7 +145,7 @@ $(document).ready(function(){
         }
         return false;
 
-    })
+    });
     //歌曲列表
     var flag=false;
     $('#btnMore').click(function(){
@@ -139,16 +157,18 @@ $(document).ready(function(){
             $('#more').css("display","none")
             flag=false;
         }
-    })
+    });
     $('#list li').click(function(){
         songIndex=$(this).val();
         console.log(songIndex);
         songEnded();
-        changeSong();
+        loadSong();
         audio.load();
         audio.play();
         document.getElementById('songPic').style.animationPlayState='running';       //音乐开始
-    })
+    });
+    //滑块拖动
+
 
 })
 
@@ -170,6 +190,7 @@ function getFullTime(){
 function getCurrentTime(){
     current=audio.currentTime;
     let m;
+
     let s;
    // console.log(current);
     m=parseInt(current/60);
@@ -179,35 +200,35 @@ function getCurrentTime(){
     //console.log(m,s);
     currentTime.text(m+":"+s).show();
     //进度条
-    let currentBar = ($('#totalBar').width() / duration) * current;
+    let currentBar =Math.round(current/duration * 10000) / 100.00 + "%";
 
-    $('#currentBar').css({
-            'width': currentBar + "px",
-        }
-    );
+    updateProgress(duration,current,currentBar);
+
+
+
 if(current==duration){
    songEnded();
    $('#nextBtn').click();
 }
 }
 //第一首歌曲载入
-function loadFirstSong(){
-    songName.text(musicList[songIndex].sing).show();
-    singerName.text(musicList[songIndex].singer).show();
-    audioJq.append("<source src='"+ musicList[songIndex].src+"' type='audio/mp3' />");
+function loadSong(){
+    songName.text(musicLists[songIndex].sing).show();
+    singerName.text(musicLists[songIndex].singer).show();
+    audioJq.append("<source src='"+ musicLists[songIndex].src+"' type='audio/mp3' />");
     console.log(songName.text());
     audio.ondurationchange=function(){ getFullTime()}; // 获得总时长
     audio.ontimeupdate=function(){ getCurrentTime()}; // 获得现时长
 }
 //下一首
-function changeSong(){
-    songName.text(musicList[songIndex].sing).show();
-    singerName.text(musicList[songIndex].singer).show();
-    audioJq.append("<source src='"+ musicList[songIndex].src+"' type='audio/mp3' />");
-    console.log(songName.text());
-    audio.ondurationchange=function(){ getFullTime()}; // 获得总时长
-    audio.ontimeupdate=function(){ getCurrentTime()}; // 获得现时长
-}
+// function loadNextSong(){
+//     songName.text(musicList[songIndex].sing).show();
+//     singerName.text(musicList[songIndex].singer).show();
+//     audioJq.append("<source src='"+ musicList[songIndex].src+"' type='audio/mp3' />");
+//     console.log(songName.text());
+//     audio.ondurationchange=function(){ getFullTime()}; // 获得总时长
+//     audio.ontimeupdate=function(){ getCurrentTime()}; // 获得现时长
+// }
 //歌曲结束
 function songEnded(){
     audioJq.empty();
@@ -219,8 +240,21 @@ function songEnded(){
 
 //歌曲列表
 function createMusicList(){
-for(var i=0;i<musicList.length;i++) {
-    $('#list').append("<li value='" + i + "'>" + musicList[i].sing + "</li>");
+for(var i=0;i<total;i++) {
+    $('#list').append("<li value='" + i + "'>" + musicLists[i].sing + "</li>");
     console.log(i);
 }
 }
+
+function updateProgress(duration,newValue,currentBar){
+    $('#totalBar').attr("max",duration);
+    totalBar.val(newValue);
+  totalBar.css('background', 'linear-gradient(to right, #ffffff, #ffffff ' + currentBar+ ', #707070)');
+}
+
+
+function slideChange(value){
+    audio.currentTime=value;
+
+}
+
